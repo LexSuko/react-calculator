ff2a4496b898dea9c2f5e123dd39c2d26862a4a6
AP <ap@APs-MacBook-Pro.local>
2022-06-06 15:06:06 +0700

    build and update code

--- src/App.js
+++ src/App.js
@@ -1,80 +1,14 @@
-import { useState } from 'react';
-import './index.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './page';
 
 function App() {
-
-  const [calc , setCalc] = useState("");
-  const [result , setResult] = useState("");
-
-  const ops = ['/','*','+','-','.'];
-
-  const updateCalc = value => {
-    if(ops.includes(value) && calc === '' || 
-    ops.includes(value) && ops.includes(calc.slice(-1))
-    ) {
-      return;
-    }
-
-    setCalc(calc + value);
-
-    if(!ops.includes(value)) {
-      setResult(eval(calc + value).toString());
-    }
-  }
-  const AC = () =>{
-    setCalc("");
-    setResult("");
-  }
-
-  const option1 = () =>{
-    setCalc(calc * -1);
-    setResult(calc * -1);
-    setCalc(eval("-"+calc).toString());
-  }
-  const option2 = () =>{
-    setCalc(calc / 100);
-    setResult(calc / 100);
-    setCalc(eval(calc).toString());
-  }
-
-  const calculate = () => {
-    setCalc(eval(calc).toString());
-  }
-
   return (
-    <div className="App">
-      <div className="calulator">
-        <div className="display">
-            {result ? <span>({result})</span> : ''}&nbsp; 
-            { calc || "0"}
-        </div>
-
-        <div className="operator">
-            <button onClick={AC}>AC</button>
-            <button onClick={option1}>+/-</button>
-            <button onClick={option2}>%</button>
-            <button onClick={() => updateCalc('/')}>/</button>
-        </div>
-        <div className="digits">
-            <button onClick={() => updateCalc('7')}>7</button>
-            <button onClick={() => updateCalc('8')}>8</button>
-            <button onClick={() => updateCalc('9')}>9</button>
-            <button onClick={() => updateCalc('*')}>X</button>
-            <button onClick={() => updateCalc('4')}>4</button>
-            <button onClick={() => updateCalc('5')}>5</button>
-            <button onClick={() => updateCalc('6')}>6</button>
-            <button onClick={() => updateCalc('-')}>-</button>
-            <button onClick={() => updateCalc('1')}>1</button>
-            <button onClick={() => updateCalc('2')}>2</button>
-            <button onClick={() => updateCalc('3')}>3</button>
-            <button onClick={() => updateCalc('+')}>+</button>
-            <button onClick={() => updateCalc('0')}>0</button>
-            <button onClick={() => updateCalc('.')}>.</button>
-
-            <button onClick={calculate}>=</button>
-        </div>
-      </div>
-    </div>
+    <Router basename={'/react-calculator'}>
+      <Routes>
+        <Route path="/" element={<Home/>} exact/>
+      </Routes>
+    </Router>
   );
 }
 
